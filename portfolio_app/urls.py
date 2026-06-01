c863cdf4df8d1c4451172365156ecf4cac0e31d2
manju1811 <valekarmanjunath4@gmail.com>
2026-06-01 18:28:53 +0530

    Added new portfolio project pages

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -15,4 +15,9 @@ urlpatterns = [
     path('portfolio/projects/', views.projects, name='projects'),
     path('portfolio/contact/', views.contact, name='contact'),
     path('projects/blog-platform/', views.blog_project, name='blog_project'),
+    path('projects/ecommerc_platform/', views.ecommerce_project,name='ecommerce_project'),
+    path('projects/todo_management/',views.todo_management,name='todo_management'),
+    path('projects/personal_system/',views.personal_system,name='personal_system'),
+    path('projects/creative_portfolio/',views.creative_portfolio,name='creative_portfolio'),
+    path('projects/',views.corporate_business,name='corporate_business'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -24,3 +24,17 @@ def contact(request):
 
 def blog_project(request):
     return render(request, 'portfolio/blog_detail.html')
+
+def ecommerce_project(request):
+    return render(request,'portfolio/ecommerce_detail.html')
+
+def todo_management(request):
+    return render(request, 'portfolio/todo_management.html')
+
+def personal_system(request):
+    return render(request,'portfolio/personal_system.html')
+
+def creative_portfolio(request):
+    return render(request,'portfolio/creative_portfolio.html')
+def corporate_business(request):
+    return render(request,'portfolio/corporate_business.html')
